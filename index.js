40990d26b3a5b752c3348a1cdaf3f92ee9ddf470
Denisse Rivas <iqrivas@gmail.com>
2021-04-24 05:32:47 -0500

    feat: added sugar level selection and start prep button

--- index.js
+++ index.js
@@ -5,13 +5,15 @@ class Order {
     }
 }
 
-let newOrder = new Order();
+let newOrder = new Order('', 0);
 
 let mainPanel = document.getElementById('main_panel');
 let orderPanel = document.getElementById('currentOrder');
 let sizePanel = document.getElementById('size_panel');
 let sugarPanel = document.getElementById('sugar_panel');
+let prepPanel = document.getElementById('prep_panel');
 let orderBtn = document.getElementById('orderBtn');
+let cancelBtn = document.getElementById('cancelBtn');
 
 function startOrder () {
     orderBtn.classList.add('hidden');
@@ -26,8 +28,15 @@ function cancelOrder () {
     orderPanel.classList.add('hidden');
     sizePanel.classList.add('hidden');
     sugarPanel.classList.add('hidden');
+    newOrder.size = '';
+    newOrder.sugar = 0;
     orderSize.innerHTML = `Size: `;
     orderSugar.innerHTML = `Sugar: `;
+    document.getElementById(`lvl-1`).classList.add('sugar_pt--off');
+    document.getElementById(`lvl-2`).classList.add('sugar_pt--off');
+    document.getElementById(`lvl-3`).classList.add('sugar_pt--off');
+    document.getElementById(`lvl-4`).classList.add('sugar_pt--off');
+    document.getElementById(`lvl-5`).classList.add('sugar_pt--off');
 }
 
 let sizeBtns = document.querySelectorAll('.size_btn');
@@ -38,5 +47,24 @@ sizeBtns.forEach(element => {
         orderSize.innerHTML = `Size: ${newOrder.size}`;
         sizePanel.classList.add('hidden');
         sugarPanel.classList.remove('hidden');
+        orderSugar.innerHTML = `Sugar: ${newOrder.sugar}`;
+    });
+})
+
+let sugarBtns = document.querySelectorAll('.sugar_btn');
+let orderSugar = document.getElementById('orderSugar');
+sugarBtns.forEach(element => {
+    element.addEventListener('click', (ev) => {
+        if (ev.target.value == 'increase' && newOrder.sugar < 5) {
+            newOrder.sugar += 1;
+            document.getElementById(`lvl-${newOrder.sugar}`).classList.remove('sugar_pt--off');
+        }
+        if (ev.target.value == 'decrease' && newOrder.sugar > 0) {
+            newOrder.sugar -= 1;
+            document.getElementById(`lvl-${newOrder.sugar+1}`).classList.add('sugar_pt--off');
+        }
+
+        orderSugar.innerHTML = `Sugar: ${newOrder.sugar}`;
+        
     });
 })
